62c77e54546e87954e72d5116fdbb66d8c2653fe
Jonas Haentjens <haentjensjonas@gmail.com>
2018-09-17 23:18:52 +0200

    Django Tutorial #21 - Login Form

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import url
 from django.urls import path
 from . import views
 
-appname = 'accounts'
+app_name = 'accounts'
 
 urlpatterns=[
-    path('signup/', views.signup_view, name="signup")
+    path('signup/', views.signup_view, name="signup"),
+    path('login/', views.login_view, name="login")
 ]
